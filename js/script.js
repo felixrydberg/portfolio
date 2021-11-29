22b85894933c94058fd3d6e71df594b86909e2bc
felixrydberg <90419848+felixrydberg@users.noreply.github.com>
2021-11-29 22:20:35 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,25 +1,62 @@
-// Nav-Dropdown Start
+function loaded() {
+    // Portfolio Start 
 
-const dropButton = document.querySelector(".nav-dropbutton")
+    let links = [
+        "#",
+        "#",
+        "#",
+        "#",
+        "#"],
+    names = [
+        "#",
+        "#",
+        "#",
+        "#",
+        "#"],
+    desc = [
+        "#",
+        "#",
+        "#",
+        "#",
+        "#"];
+    imgname = [
+        "logo.png",
+        "logo.png",
+        "logo.png",
+        "logo.png",
+        "logo.png"];
 
-dropButton.addEventListener("click", navdropdown);
+    // spawnPortfolio(links, names, desc, imgname);
 
-function navdropdown() {
-    document.getElementById("nav-dropcontent").classList.toggle("display-block");
+    // function spawnPortfolio(links, names, desc, imgname) {
+    //     const portPar = document.querySelector(".portfolio");
+    //     if (links.length == names.length && links.length == desc.length && links.length == imgname.length) {
+    //         for(let i = 0; i < links.length; i++) {
+    //             const article = document.createElement("article"),
+    //             img = document.createElement("img"),
+    //             h1 = document.createElement("h1"),
+    //             p = document.createElement("p");
+    //             let color = (20 / links.length * i) + 30; 
+    //             article.style.background =`hsl (0, 0%, 30%)`;
+    //             portPar.appendChild(article);
+    //             article.appendChild(img)
+    //             article.appendChild(h1);
+    //             article.appendChild(p);
+    //             img.setAttribute('src', 'image/portfolio/' + imgname[i]);
+    //             h1.innerHTML = names[i];
+    //             p.innerHTML = desc[i];
+    //             console.log(color);
+    //         }
+    //     }
+    //     else {
+    //         console.log("Array lenght failure");
+    //     }
+    // }
+
+    // Lightmode 
+
+    
+
 }
-window.onclick = function(event) {
-    console.log("lmao onclick window")
-    if (!event.target.matches('.nav-dropbutton')) {
-        var dropdowns = document.getElementsByClassName("nav-dropcontent");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('display-block')) {
-            openDropdown.classList.remove('display-block');
-        }
-        }
-    }
-}
 
-
-// 
+loaded()
