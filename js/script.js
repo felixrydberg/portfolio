a44ad218acedd590ee9b9a5a57bf25bd2461a1b3
felixrydberg <felixryd@gmail.com>
2022-04-01 22:59:36 +0200

    [Add] Lightmode toggle

--- js/script.js
+++ js/script.js
@@ -1,62 +1,49 @@
-function loaded() {
-    // Portfolio Start 
+(function () {
+  (function () {
+    const btn = document.querySelector('#light-mode');
+    const btnBackground = document.querySelector('#btn-background');
 
-    let links = [
-        "#",
-        "#",
-        "#",
-        "#",
-        "#"],
-    names = [
-        "#",
-        "#",
-        "#",
-        "#",
-        "#"],
-    desc = [
-        "#",
-        "#",
-        "#",
-        "#",
-        "#"];
-    imgname = [
-        "logo.png",
-        "logo.png",
-        "logo.png",
-        "logo.png",
-        "logo.png"];
+    const colors = [
+      {
+        color: 'black',
+        background: 'rgb(255, 254, 240)',
+      },
+      {
+        color: 'white',
+        background: 'rgb(22, 22, 21)',
+      },
+    ];
 
-    // spawnPortfolio(links, names, desc, imgname);
+    let drkmode = false;
+    btn.addEventListener('click', () => {
+      if (drkmode) {
+        drkmode = false;
+        btn.style.backgroundImage = "url('../image/icon/moon.svg')";
+        changeColor(colors[1]);
+      } else {
+        drkmode = true;
+        btn.style.backgroundImage = "url('../image/icon/sun.svg')";
+        changeColor(colors[0]);
+      }
+    });
+  })();
 
-    // function spawnPortfolio(links, names, desc, imgname) {
-    //     const portPar = document.querySelector(".portfolio");
-    //     if (links.length == names.length && links.length == desc.length && links.length == imgname.length) {
-    //         for(let i = 0; i < links.length; i++) {
-    //             const article = document.createElement("article"),
-    //             img = document.createElement("img"),
-    //             h1 = document.createElement("h1"),
-    //             p = document.createElement("p");
-    //             let color = (20 / links.length * i) + 30; 
-    //             article.style.background =`hsl (0, 0%, 30%)`;
-    //             portPar.appendChild(article);
-    //             article.appendChild(img)
-    //             article.appendChild(h1);
-    //             article.appendChild(p);
-    //             img.setAttribute('src', 'image/portfolio/' + imgname[i]);
-    //             h1.innerHTML = names[i];
-    //             p.innerHTML = desc[i];
-    //             console.log(color);
-    //         }
-    //     }
-    //     else {
-    //         console.log("Array lenght failure");
-    //     }
-    // }
+  const changeColor = (colors) => {
+    document.body.style.background = colors.background;
 
-    // Lightmode 
+    let elements = [
+      document.querySelectorAll('li'),
+      document.querySelectorAll('li a'),
+      document.querySelectorAll('p'),
+      document.querySelectorAll('h1'),
+    ];
 
-    
+    for (const key of elements) {
+      for (const element of key) {
+        element.style.color = colors.color;
+      }
+    }
 
-}
-
-loaded()
+    document.querySelector('.banner article p').style.color = 'black';
+  };
+})();
